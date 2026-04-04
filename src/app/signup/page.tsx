d5cc5793cf4ed7cbf74c1bc20462fd9b5bbbed60
Shivam Sharma <shivamcodesss@gmail.com>
2026-04-04 14:03:48 +0530

    completed the signup page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,9 +2,6 @@
 import React, { useState } from 'react'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
-import axios from 'axios';
-
-
 import { Button } from "@/components/ui/button"
 import {
   Card,
@@ -17,13 +14,45 @@ import {
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import toast from 'react-hot-toast';
 const page = () => {
     const router=useRouter();
     const[email,setEmail]=useState("");
     const[password,setPassword]=useState("");
+    const[loading,setLoading]=useState(false);
+    async function onSignup(e : any){
+      e.preventDefault();
+      try{
+        setLoading(true);
+        const response=await fetch("/api/users/signup",{
+          method: "POST",
+          headers:{"Content-Type" : "application/json"},
+          body: JSON.stringify({email,password})
+        })
+        const data=await response.json();
+        console.log(response);
+        console.log(data);
+        if(!response.ok){
+          setLoading(false);
+          for(let err of data.error){
+            toast.error(err);
+          }
+        }
+        else{
+          setLoading(false);
+          toast.success(data.msg);
+          router.push('/login');
+        }
+      }
+      catch(error){
+        console.log(error);
+        toast.error("Something went wrong");
+        setLoading(false);
+      }
+    }
   return (
     <div className='bg-stone-900 h-screen w-full text-white flex items-center justify-center'>
-         <Card className="w-full max-w-sm flex">
+          <Card className="w-full max-w-sm flex">
       <CardHeader>
         <CardTitle>Signup to your account</CardTitle>
         <CardDescription>
@@ -33,8 +62,8 @@ const page = () => {
           <Link href={'/login'}>Login</Link>
         </CardAction>
       </CardHeader>
-      <CardContent>
-        <form>
+      <form  onSubmit={onSignup}>
+        <CardContent>
           <div className="flex flex-col gap-6">
             <div className="grid gap-2">
               <Label htmlFor="email">Email</Label>
@@ -54,13 +83,15 @@ const page = () => {
               <Input id="password" placeholder='****' type="password" required value={password} onChange={(e)=>setPassword(e.target.value)} />
             </div>
           </div>
-        </form>
       </CardContent>
-      <CardFooter className="flex-col gap-2">
-        <Button type="submit" className="w-full">
-          Signup
+      <CardFooter className="flex-col gap-2 mt-4">
+        <Button type="submit" className="w-full" disabled={loading}>
+          {
+          loading ? "... Signing up" : "Signup"
+          }
         </Button>
-      </CardFooter>
+      </CardFooter> 
+      </form>
     </Card>
     </div>
   )
